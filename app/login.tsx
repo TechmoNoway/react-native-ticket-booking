9e698f820f260b7019978951a525f6d2647e6412
Ky Nguyen <cuty0406@outlook.com>
2025-04-26 22:08:41 +0700

    Create some ui components and start login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,3 +1,68 @@
+import { HStack } from "@/components/HStack";
+import { Input } from "@/components/Input";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Text } from "@/components/Text";
+import { VStack } from "@/components/VStack";
+import { useState } from "react";
+import { KeyboardAvoidingView, ScrollView } from "react-native";
+
 export default function Login() {
-  return <></>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flex: 1 }}>
+        <VStack
+          flex={1}
+          justifyContent="center"
+          alignItems="center"
+          p={40}
+          gap={40}
+        >
+          <HStack gap={10}>
+            <Text fontSize={30} bold mb={20}>
+              Ticket Booking
+            </Text>
+            <TabBarIcon name="ticket" size={50} />
+          </HStack>
+
+          <VStack w={"100%"} gap={30}>
+            <VStack gap={5}>
+              <Text ml={10} fontSize={14} color="gray">
+                Email
+              </Text>
+              <Input
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Email"
+                placeholderTextColor="darkgray"
+                autoCapitalize="none"
+                autoCorrect={false}
+                h={48}
+                p={14}
+              />
+            </VStack>
+
+            <VStack gap={5}>
+              <Text ml={10} fontSize={14} color="gray">
+                Password
+              </Text>
+              <Input
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Password"
+                placeholderTextColor="darkgray"
+                autoCapitalize="none"
+                autoCorrect={false}
+                h={48}
+                p={14}
+              />
+            </VStack>
+          </VStack>
+        </VStack>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
